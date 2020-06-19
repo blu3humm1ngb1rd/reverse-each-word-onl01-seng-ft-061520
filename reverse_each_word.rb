60abad92a662c5919993d103b39304cbbabfcda2
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-19 00:08:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(string)
   end.join(" ") 
   
 end
+
+def reverse_each_word(string2)
+  new_string = string2.split
+  new_string.collect do |rev| 
+     rev.reverse
+  end.join(" ") 
+  
+end
